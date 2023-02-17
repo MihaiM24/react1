db181c0fd1efe0ee1ba5f262f02b9dcc0d0bfd51
Mihai Mitricel <mihaimitricel24@gmail.com>
2023-02-17 08:57:24 +0200

    ex3

--- ex3/src/index.js
+++ ex3/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const helloworld = <h1>Hello, World!</h1>;
-
+function calc (a,b){
+  return <h2>{a+b}</h2>
+}
 const root = ReactDOM.createRoot(document.querySelector('#root'));
-root.render(helloworld);
-
+root.render(calc(1,2))
